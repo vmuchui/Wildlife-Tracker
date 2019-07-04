dd2dd84e2ed15631a0ea869f9fb79cc62ca2f092
vmuchui <kaigongimuchui@gmail.com>
2019-07-04 13:05:44 +0300

    add initial file.

--- src/test/java/PlaceTest.java
+++ src/test/java/PlaceTest.java
@@ -1,5 +1,37 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlaceTest {
+    @Test
+    public void Place_instatiatesCorrectly_True() {
+        Place newPlace = new Place("river");
+        assertTrue(newPlace instanceof Place);
+    }
+    @Test
+    public void getName() {
+        Place location = new Place("river");
+        assertEquals("river", location.getName());
+    }
 
+    @Test
+    public void setName() {
+        Place place = new Place("river");
+        place.setName("doof");
+        assertEquals("doof", place.getName());
+    }
+
+    @Test
+    public void getId() {
+        Place place = new Place("river");
+        place.setId(2);
+        assertEquals(2,place.getId());
+    }
+
+    @Test
+    public void setId() {
+        Place place = new Place("river");
+        place.setId(3);
+        assertEquals(3,place.getId());
+    }
 }
