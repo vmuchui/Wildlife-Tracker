e99020c83f9bbc5711b2474153c5352ccc6bc5fc
vmuchui <kaigongimuchui@gmail.com>
2019-07-04 13:01:01 +0300

    add POJO with a constructor and getter-setter methods

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import org.sql2o.*;
 
-public class Ranger {
+public class Ranger implements Wildlife{
+
+
+    private String name;
+    private String badge;
+    private int id;
+
+    public Ranger(String name, String badge) {
+        this.name = name;
+        this.badge = badge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public void delete() {
+
+    }
+
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public void save() {
+        try (Connection connection = DB.sql2o.open()){
+            String sql = "INSERT INTO rangers (name, badge) VALUES (:name, :badge);";
+
+            this.id= (int) connection.createQuery(sql).executeUpdate().getKey();
+        }
+    }
 }
