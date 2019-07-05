d9bbae669258f1e80a39995a4d080601ee6c27ae
vmuchui <kaigongimuchui@gmail.com>
2019-07-05 10:26:40 +0300

    add tests for delete method for the Place class. add initial file for sightings class. add test for save method for Place class. add implementation of delete and save methods. add test for find method and its implementation in the Place class.

--- src/main/java/Place.java
+++ src/main/java/Place.java
@@ -1,4 +1,9 @@
-public class Place {
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Place implements Wildlife{
     private String name;
     private int id;
 
@@ -22,6 +27,51 @@ public class Place {
         this.name = name;
     }
 
+    public static List<Place> all() {
+        try (Connection conn = DB.sql2o.open()){
+            String sql = "SELECT * FROM places;";
+            return conn.createQuery(sql).executeAndFetch(Place.class);
+        }
+    }
+
+    public static Place find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM places WHERE id=:id;";
+           return con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Place.class);
+        }
+    }
+
+    @Override
+    public void delete() {
+        try (Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM places * WHERE id = :id;";
+            con.createQuery(sql).addParameter("id",this.id).executeUpdate();
+        }
+    }
+
+    @Override
+    public void save() {
+        try (Connection connection = DB.sql2o.open()){
+            String sql = "INSERT INTO places (name) VALUES(:name);";
+            this.id = (int) connection.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .executeUpdate().getKey();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Place)) return false;
+        Place place = (Place) o;
+        return getId() == place.getId() &&
+                getName().equals(place.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getId());
+    }
 }
 
 
--- src/test/java/PlaceTest.java
+++ src/test/java/PlaceTest.java
@@ -1,8 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class PlaceTest {
+    @Before
+    public void setUp() throws Exception {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "victor", "Am0skwito");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        try (Connection con = DB.sql2o.open()){
+            String initialize = "DELETE FROM places *;";
+            con.createQuery(initialize).executeUpdate();
+        }
+    }
     @Test
     public void Place_instatiatesCorrectly_True() {
         Place newPlace = new Place("river");
@@ -33,5 +49,32 @@ public class PlaceTest {
         Place place = new Place("river");
         place.setId(3);
         assertEquals(3,place.getId());
+    }
+
+
+    @Test
+    public void delete() {
+      Place place = new Place("river");
+      place.save();
+      Place newplace = new Place("north");
+      newplace.save();
+      place.delete();
+      assertFalse(Place.all().contains(place));
+      assertTrue(Place.all().contains(newplace));
+    }
+
+    @Test
+    public void save() {
+        Place place = new Place("river");
+        place.save();
+        assertTrue(Place.all().contains(place));
+    }
+
+    @Test
+    public void find() {
+        Place place = new Place("river");
+        place.save();
+        Place newPlace = Place.find(place.getId());
+        assertEquals(place,newPlace);
     }
 }
