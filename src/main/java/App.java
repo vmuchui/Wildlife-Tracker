466355a3e72753a272d87a9d9791745ff676d5a9
vmuchui <kaigongimuchui@gmail.com>
2019-07-05 13:27:13 +0300

    change the position of the templates folder in the directory structure.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        
+
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
