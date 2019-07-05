e535baee0d7ee4ac2408d9829812dff45945eebd
vmuchui <kaigongimuchui@gmail.com>
2019-07-05 10:45:40 +0300

    add blueprint for a sighting object. add tests for the getter methods, setter methods, constructor, save and delete.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,3 +1,72 @@
-public class Sighting {
+import java.util.Objects;
 
+public class Sighting implements Wildlife{
+private int id;
+private int placeid;
+private int animalid;
+private int rangerid;
+
+    public Sighting(int placeid, int animalid, int rangerid) {
+        this.placeid = placeid;
+        this.animalid = animalid;
+        this.rangerid = rangerid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlaceid() {
+        return placeid;
+    }
+
+    public void setPlaceid(int placeid) {
+        this.placeid = placeid;
+    }
+
+    public int getAnimalid() {
+        return animalid;
+    }
+
+    public void setAnimalid(int animalid) {
+        this.animalid = animalid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Sighting)) return false;
+        Sighting sighting = (Sighting) o;
+        return getId() == sighting.getId() &&
+                getPlaceid() == sighting.getPlaceid() &&
+                getAnimalid() == sighting.getAnimalid() &&
+                getRangerid() == sighting.getRangerid();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPlaceid(), getAnimalid(), getRangerid());
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    public int getRangerid() {
+        return rangerid;
+    }
+
+    public void setRangerid(int rangerid) {
+        this.rangerid = rangerid;
+    }
 }
---
+++ src/test/java/SightingTest.java
@@ -0,0 +1,56 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SightingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getPlaceid() {
+    }
+
+    @Test
+    public void setPlaceid() {
+    }
+
+    @Test
+    public void getAnimalid() {
+    }
+
+    @Test
+    public void setAnimalid() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void getRangerid() {
+    }
+
+    @Test
+    public void setRangerid() {
+    }
+}
