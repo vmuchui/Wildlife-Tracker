e14fb8e579b6312ce3f6d9051ddaa84ca791a72b
vmuchui <kaigongimuchui@gmail.com>
2019-07-06 19:59:01 +0300

    add deployment dependencies.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: .build/install/animals-wildlife/bin/wildlife-tracker
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,6 @@ import org.sql2o.Sql2o;
 
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "victor", "Am0skwito");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "victor", "Am0skwito");
+    public static Sql2o sql2o = new Sql2o("postgres://bwvuownllrdgde:d6358a75d5e651cb33552ee0ee44e7d42ae0852544a6a6538aba4bd8630bf7e3@ec2-23-21-160-38.compute-1.amazonaws.com:5432/d321fv2jsuelid", "bwvuownllrdgde", "d6358a75d5e651cb33552ee0ee44e7d42ae0852544a6a6538aba4bd8630bf7e3");
 }
